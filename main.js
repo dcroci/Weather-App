d590207766928bd90ea03253e02a4cb34ba8dcde
Dominik Croci <dominikrcroci@gmail.com>
2023-08-07 07:38:33 -0700

    umbrella feature + css

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
 const card = document.querySelector('.card');
-const img = document.querySelector('img');
+const img = document.querySelector('.bottoms-img');
 const cityName = document.querySelector('.city-name');
 const currentTemp = document.querySelector('.current-temp');
 const bottomsText = document.querySelector('.bottoms-text');
 const cityInput = document.querySelector('#city');
 const btn = document.querySelector('button');
+const umbrellaCard = document.querySelector('.umbrella');
+const umbrellaQ = document.querySelector('.umbrella-q');
+const weatherCondition = document.querySelector('.weather-cond');
+const umbrellaImg = document.querySelector('.umbrella-img');
+const weatherDesc = document.querySelector('.weather-desc');
+const sunnyConditions = ['Sunny', 'Clear', 'Partly cloudy'];
 let cityValue = '';
 const apiKey = '3da167d533614b87a29175633230608';
-let apiData = {};
-const updateCard = (data) => {
+let apiData;
+const updateCards = (data) => {
   if (card.classList.contains('hidden')) {
     card.classList.remove('hidden');
   }
   cityName.textContent = `${data.location.name}, ${data.location.region} `;
   currentTemp.textContent = `${data.current.temp_f} Degrees F°`;
-  if (data.current.temp_f > 90) {
+  if (data.current.temp_f > 80) {
     img.src = 'shorts.png';
     bottomsText.textContent = 'You should wear shorts today!';
   } else {
     img.src = 'pants.png';
     bottomsText.textContent = 'It is definitely a pants day!';
   }
+
+  weatherCondition.textContent = `It is currently ${data.current.condition.text} outside!`;
+  umbrellaImg.src = `${data.current.condition.icon}`;
+  for (let i = 0; i < sunnyConditions.length; i++) {
+    if (data.current.condition.text === sunnyConditions[i]) {
+      weatherDesc.textContent =
+        'Only bring an umbrella if you plan on blocking the sun!';
+      break;
+    } else {
+      weatherDesc.textContent = 'An umbrella might be a good idea!';
+    }
+  }
 };
 const resetInput = () => {
   cityInput.value = '';
 };
 
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  cityValue = cityInput.value;
+const fetchData = (cityValue) => {
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityValue}&aqi=yes`
   )
@@ -40,10 +56,23 @@ btn.addEventListener('click', (e) => {
     })
     .then((data) => {
       console.log(data);
-      updateCard(data);
+      apiData = data;
+      updateCards(apiData);
     })
     .catch((error) => {
       console.error('Error fetching data:', error);
     });
+};
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  cityValue = cityInput.value;
+  fetchData(cityValue);
   resetInput();
+});
+
+umbrellaQ.addEventListener('click', () => {
+  if (umbrellaCard.classList.contains('hidden')) {
+    umbrellaCard.classList.remove('hidden');
+  }
 });
